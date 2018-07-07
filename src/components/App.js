fae9fbfab3ae5c1a1855689ac44de844302e13bb
Mustafa Salih <mustafa@mustafa.local>
2018-07-07 11:12:26 -0700

    starter pack

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../sass/main.scss';
+
+class App extends React.Component {
+    render() {
+        return <h1>Hello World</h1>
+    }
+}
+
+export default App;
+
+
+
